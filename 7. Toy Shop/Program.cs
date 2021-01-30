4170118fdebac6c256b6adbd258463bfe40ee271
Момчил Миланов <deadman_kula@abv.bg>
2021-01-30 18:25:33 +0200

    Add project files.

---
+++ 7. Toy Shop/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace _7._Toy_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Петя има магазин за детски играчки. Тя получава голяма поръчка, която трябва да изпълни.С парите, които ще спечели иска да отиде на екскурзия.Да се напише програма, която пресмята печалбата от поръчката.
+            //Цени на играчките:
+            //⦁	Пъзел - 2.60 лв.
+            //⦁	Говореща кукла -3 лв.
+            //⦁	Плюшено мече -4.10 лв.
+            //⦁	Миньон - 8.20 лв.
+            //⦁	Камионче - 2 лв.
+            //Ако поръчаните играчки са 50 или повече магазинът прави отстъпка 25 % от общата цена. От спечелените пари Петя трябва да даде 10 % за наема на магазина.Да се пресметне дали парите ще ѝ стигнат да отиде на екскурзия.
+            //От конзолата се четат 6 реда:
+            //⦁	Цена на екскурзията - реално число в интервала[1.00 … 10000.00]
+            double priceOfHoliday = double.Parse(Console.ReadLine());
+            //⦁	Брой пъзели -цяло число в интервала[0… 1000]
+            int numOfPuzzles = int.Parse(Console.ReadLine());
+            //⦁	Брой говорещи кукли - цяло число в интервала[0 … 1000]
+            int numOfToys = int.Parse(Console.ReadLine());
+            //⦁	Брой плюшени мечета - цяло число в интервала[0 … 1000]
+            int numOfBears = int.Parse(Console.ReadLine());
+            //⦁	Брой миньони -цяло число в интервала[0 … 1000]
+            int numOfMinions = int.Parse(Console.ReadLine());
+            //⦁	Брой камиончета -цяло число в интервала[0 … 1000]
+            int numOfTrucs = int.Parse(Console.ReadLine());
+
+            double totalSum = numOfPuzzles * 2.60 + numOfToys * 3 + numOfBears * 4.10 + numOfMinions * 8.20 + numOfTrucs * 2;
+            totalSum -= totalSum * 0.10;
+            int totalNumOfToys = numOfBears + numOfMinions + numOfPuzzles + numOfToys + numOfTrucs;
+            if (totalNumOfToys >= 50)
+            {
+                totalSum -= totalSum * 0.25;
+            }
+
+            if (totalSum >= priceOfHoliday)
+            {
+                Console.WriteLine($"Yes! {totalSum - priceOfHoliday:f2} lv left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! {priceOfHoliday - totalSum:f2} lv needed.");
+            }
+
+
+
+            //На конзолата се отпечатва:
+            //⦁	Ако парите са достатъчни се отпечатва:
+            //⦁	"Yes! {оставащите пари} lv left."
+            //⦁	Ако парите НЕ са достатъчни се отпечатва:
+            //⦁	"Not enough money! {недостигащите пари} lv needed."
+            //Резултатът трябва да се форматира до втория знак след десетичната запетая.
+
+
+
+        }
+    }
+}
---
+++ mishoTest/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace mishoTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "България",
+            "Русия",
+            "Франция"});
+            this.comboBox1.Location = new System.Drawing.Point(13, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "София",
+            "Пловдив",
+            "Варна"});
+            this.comboBox2.Location = new System.Drawing.Point(141, 13);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(269, 13);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 116);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+    }
+}
+
